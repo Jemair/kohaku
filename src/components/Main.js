a8137d111e12db196f1c7354ae53c392d55df922
Liam.Wang <is.liamwang@gmail.com>
2017-07-15 16:56:14 +0800

    background

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,15 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
-import React from 'react';
+import React, {Component} from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+const background = require('../images/background.mp4');
 
-class AppComponent extends React.Component {
+class AppComponent extends Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator"/>
-        <div className="not1ice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <video src={background} loop autoPlay></video>
       </div>
     );
   }
